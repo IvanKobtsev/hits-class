5cad5a50a07a2985275e8510b980573e75061b4e
Ivan Kobtsev <ivan.kobtsev@mcc-soft.de>
2026-02-16 20:53:49 +0700

    Added Id to CommentDto

--- webapi/src/Team13.HitsClass.App/Features/Comments/Dto/CommentDto.cs
+++ webapi/src/Team13.HitsClass.App/Features/Comments/Dto/CommentDto.cs
@@ -4,8 +4,10 @@ namespace Team13.HitsClass.App.Features.Comments.Dto;
 
 public class CommentDto
 {
-    public UserDto Author { get; set; }
-    public string TextLexical { get; set; }
+    public int Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? LastEditedAt { get; set; }
+
+    public UserDto Author { get; set; }
+    public string TextLexical { get; set; }
 }
