34e31f242135f7fd0cd8dd1666d2657a8c3aabd8
Sofya Sidonskaya <sonyasidonya@gmail.com>
2026-02-16 21:01:34 +0700

    added dtos

---
+++ webapi/src/Team13.HitsClass.App/Features/Assignment/Dto/AssignmentDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team13.HitsClass.App.Features.Assignment.Dto
+{
+    public class AssignmentDto
+    {
+        public Guid Id { get; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public User Author { get; set; }
+        public DateTime? DeadlineUTC { get; set; }
+        public DateTime CreatedAtUTC { get; set; }
+        public DateTime LastUpdatedAtUTC { get; set; }
+        public List<FileInfoDto> Attachments { get; set; }
+        // public List<Comment> Comments { get; set; }
+    }
+}
