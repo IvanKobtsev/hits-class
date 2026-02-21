dfe4c6883ca33f94d9bee61d0fa5a52dd1652b1f
Ilya_Uymanov <ninjaxxxflame@gmail.com>
2026-02-21 18:08:42 +0700

    Refactored LoginPage a little; Set up testing; Covered LoginPage.tsx with tests (tdd design got violated, because the page was in the template of the project).

--- frontend/src/pages/unauthorized/LoginPage.tsx
+++ frontend/src/pages/unauthorized/LoginPage.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import styles from './LoginPage.module.scss';
-import { QueryFactory } from 'services/api';
-import { format } from 'date-fns';
 import { useScopedTranslation } from 'application/localization/useScopedTranslation';
-import clsx from 'clsx';
 import { Button } from 'components/uikit/buttons/Button';
 import { Field } from 'components/uikit/Field';
 import { Input } from 'components/uikit/inputs/Input';
@@ -13,10 +10,6 @@ import { FormError } from 'components/uikit/FormError';
 import { Loading } from 'components/uikit/suspense/Loading';
 import { handleLoginErrors, sendLoginRequest } from 'helpers/auth/auth-client';
 import { queryClient } from 'services/api/query-client-helper';
-import {
-  openExternalLoginPopup,
-  openExternalLoginRedirect,
-} from 'helpers/auth/openid/openid-manager';
 
 type Form = {
   login: string;
@@ -25,9 +18,6 @@ type Form = {
 
 export const LoginPage: React.FC = (props) => {
   const [isLoading, setIsLoading] = useState(false);
-  const serverVersionQuery = QueryFactory.VersionQuery.useVersionQuery({
-    throwOnError: false,
-  });
   const i18n = useScopedTranslation('Page.Login');
   const form = useAdvancedForm<Form>(async (data) => {
     setIsLoading(true);
@@ -44,18 +34,7 @@ export const LoginPage: React.FC = (props) => {
     <div className={styles.root}>
       <div className={styles.gridContainer}>
         <div className={styles.mainBackground}></div>
-        <div className={styles.infoContainer}>
-          <div className={styles.appName}>HitsClass</div>
-          <div className={styles.appInfo}>
-            <span>
-              © 2016—<span>{format(new Date(), 'yyyy')}</span> MCC Soft GmbH +
-              Co. KG
-            </span>
-            <span>
-              Server-Version: <span>{serverVersionQuery.data}</span>
-            </span>
-          </div>
-        </div>
+        <div className={styles.appName}>{i18n.t('app_name')}</div>
         <div className={styles.loginContainer} data-test-id={'login-container'}>
           <Loading loading={isLoading}>
             <form onSubmit={form.handleSubmitDefault}>
@@ -69,7 +48,6 @@ export const LoginPage: React.FC = (props) => {
                   errorText={form.formState.errors.login?.message}
                 />
               </Field>
-
               <Field
                 title={i18n.t('password_field')}
                 className={styles.inputField}
@@ -90,30 +68,6 @@ export const LoginPage: React.FC = (props) => {
               </FormError>
               <div className={styles.buttonContainer}>
                 <Button title={i18n.t('login_button')} type="submit" />
-                <Button
-                  title={'Google'}
-                  onClick={async () => {
-                    try {
-                      setIsLoading(true);
-                      await openExternalLoginRedirect('Google');
-                    } catch (e) {
-                    } finally {
-                      setIsLoading(false);
-                    }
-                  }}
-                />
-                <Button
-                  title={'GooglePopup'}
-                  onClick={async () => {
-                    try {
-                      setIsLoading(true);
-                      await openExternalLoginPopup('Google');
-                    } catch (e) {
-                    } finally {
-                      setIsLoading(false);
-                    }
-                  }}
-                />
               </div>
             </form>
           </Loading>
