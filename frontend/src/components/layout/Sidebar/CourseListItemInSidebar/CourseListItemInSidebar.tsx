993ad6d7448124f8a55dad155bc4a2088fb91832
Ilya_Uymanov <ninjaxxxflame@gmail.com>
2026-03-07 21:30:56 +0700

    CourseListItemInSidebar GREEN

--- frontend/src/components/layout/Sidebar/CourseListItemInSidebar/CourseListItemInSidebar.tsx
+++ frontend/src/components/layout/Sidebar/CourseListItemInSidebar/CourseListItemInSidebar.tsx
@@ -1,9 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
 import { CourseListItemDto } from 'services/api/api-client.types';
 
 export type CourseListItemInSidebarProps = {
   course: CourseListItemDto;
 };
 
-export const CourseListItemInSidebar: React.FC<CourseListItemInSidebarProps> = (
-  props: CourseListItemInSidebarProps,
-) => <div></div>;
+export const CourseListItemInSidebar: React.FC<CourseListItemInSidebarProps> = ({
+  course,
+}) => (
+  <Link to={`/course/${course.id}`}>{course.title}</Link>
+);
