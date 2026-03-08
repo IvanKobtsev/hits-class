a79dc9ffd34a9608eff9a983cae71cef4b30f215
Ilya_Uymanov <ninjaxxxflame@gmail.com>
2026-03-08 10:17:56 +0700

    RootPage / AppHeader fix

--- frontend/src/pages/authorized/RootPage.tsx
+++ frontend/src/pages/authorized/RootPage.tsx
@@ -4,15 +4,19 @@ import styles from './RootPage.module.scss';
 import { ModalProvider } from 'components/uikit/modal/useModal.tsx';
 import { AppHeader } from 'components/layout/Header/AppHeader';
 import { SidebarProvider } from 'components/layout/Sidebar/SidebarContext';
+import { Sidebar } from 'components/layout/Sidebar/Sidebar';
 
 export const RootPage: React.FC = () => {
   return (
     <ModalProvider>
       <SidebarProvider>
         <div className={styles.container} data-test-id="main-page-container">
+          <AppHeader />
           <div className={styles.content}>
-            <AppHeader />
-            <Outlet />
+            <Sidebar />
+            <main className={styles.main}>
+              <Outlet />
+            </main>
           </div>
         </div>
       </SidebarProvider>
