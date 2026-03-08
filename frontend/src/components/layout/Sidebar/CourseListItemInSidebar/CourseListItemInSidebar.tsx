70979c4d6731c57172e16f7c9069126af67d5655
Ilya_Uymanov <ninjaxxxflame@gmail.com>
2026-03-08 10:16:01 +0700

    CourseListItemInSidebar REFACTOR

--- frontend/src/components/layout/Sidebar/CourseListItemInSidebar/CourseListItemInSidebar.tsx
+++ frontend/src/components/layout/Sidebar/CourseListItemInSidebar/CourseListItemInSidebar.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { CourseListItemDto } from 'services/api/api-client.types';
+import styles from './CourseListItemInSidebar.module.scss';
+
+const COLOR_COUNT = 7;
 
 export type CourseListItemInSidebarProps = {
   course: CourseListItemDto;
@@ -9,5 +12,13 @@ export type CourseListItemInSidebarProps = {
 export const CourseListItemInSidebar: React.FC<CourseListItemInSidebarProps> = ({
   course,
 }) => (
-  <Link to={`/course/${course.id}`}>{course.title}</Link>
+  <Link to={`/course/${course.id}`} className={styles.item}>
+    <span
+      className={styles.avatar}
+      data-color-index={course.id % COLOR_COUNT}
+    >
+      {course.title[0].toUpperCase()}
+    </span>
+    <span className={styles.title}>{course.title}</span>
+  </Link>
 );
