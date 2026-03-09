e0b8a3a4097ecfb254939bf7b01d4df3dff91e8a
Ivan Kobtsev <ivan.kobtsev@mcc-soft.de>
2026-03-09 08:20:01 +0700

    Updated AssignmentController and DTOs

--- webapi/src/Team13.HitsClass.App/Features/Assignment/AssignmentController.cs
+++ webapi/src/Team13.HitsClass.App/Features/Assignment/AssignmentController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Team13.HitsClass.App.Features.Assignment.Dto;
+using Team13.HitsClass.App.Features.Publications.Dto;
 
 namespace Team13.HitsClass.App.Features.Assignment
 {
@@ -18,36 +19,32 @@ namespace Team13.HitsClass.App.Features.Assignment
         /// Gets statistics for specific assignment
         /// </summary>
         [HttpGet("{id:int}/statistics")]
-        public async Task<AssignmentStatisticDto> GetAssignmentStatistics([FromRoute] int id)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<AssignmentStatisticDto> GetAssignmentStatistics([FromRoute] int id) =>
+            await _assignmentService.GetAssignmentStatistics(id);
 
         /// <summary>
         /// Create assignment (check permission)
         /// </summary>
-        [HttpPost]
-        public async Task<AssignmentDto> CreateAssignment([FromBody] CreateAssignmentDto dto)
-        {
-            throw new NotImplementedException();
-        }
+        [HttpPost("/api/courses/{courseId:int}/assignments")]
+        public async Task<PublicationDto> CreateAssignment(
+            [FromRoute] int courseId,
+            [FromBody] CreateAssignmentDto dto
+        ) => await _assignmentService.CreateAssignment(courseId, dto);
 
         /// <summary>
         /// Update specific assignment (check permission)
         /// </summary>
-        [HttpPut("{id:int}")]
-        public async Task UpdateAssignment([FromRoute] int id, [FromBody] CreateAssignmentDto dto)
-        {
-            throw new NotImplementedException();
-        }
+        [HttpPut("{assignmentId:int}")]
+        public async Task<PublicationDto> PatchAssignment(
+            [FromRoute] int assignmentId,
+            [FromBody] PatchAssignmentDto dto
+        ) => await _assignmentService.PatchAssignment(assignmentId, dto);
 
         /// <summary>
         /// Delete specific assignment (check permission)
         /// </summary>
-        [HttpDelete("{id:int}")]
-        public async Task DeleteAssignment([FromRoute] int id)
-        {
-            throw new NotImplementedException();
-        }
+        [HttpDelete("{assignmentId:int}")]
+        public async Task DeleteAssignment([FromRoute] int assignmentId) =>
+            await _assignmentService.DeleteAssignment(assignmentId);
     }
 }
---
+++ webapi/src/Team13.HitsClass.App/Features/Assignment/Dto/PatchAssignmentPayloadDto.cs
@@ -0,0 +1,13 @@
+﻿using Team13.HitsClass.Domain;
+using Team13.WebApi.Domain.Helpers;
+using Team13.WebApi.Patching.Models;
+
+namespace Team13.HitsClass.App.Features.Assignment.Dto;
+
+public class PatchAssignmentPayloadDto : PatchRequest<PublicationPayload>
+{
+    [RequiredOrMissing]
+    public string Title { get; set; }
+
+    public DateTime? DeadlineUtc { get; set; }
+}
