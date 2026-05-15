2d7e2fe4a2477f37ddc31fc36806e6dcb2cdfdf9
Polina Adashkevich <p.adashkevich@skyro.io>
2026-05-15 11:58:17 +0700

    add min + max mark to team assignment view

--- frontend/src/pages/authorized/TeamAssignmentPage/TeamAssignmentView/TeamAssignmentView.tsx
+++ frontend/src/pages/authorized/TeamAssignmentPage/TeamAssignmentView/TeamAssignmentView.tsx
@@ -35,7 +35,7 @@ export const TeamAssignmentView = ({
 }: AssignmentViewProps) => {
   const { content, author, createdAtUTC: createdAtUTCRaw } = assignment;
   const createdAtUTC = new Date(createdAtUTCRaw);
-  const { title, deadlineUtc: deadlineUtcRaw } =
+  const { title, deadlineUtc: deadlineUtcRaw, markType, minMark, maxMark } =
     assignment.publicationPayload as AssignmentPayload;
   const deadlineUtc = deadlineUtcRaw ? new Date(deadlineUtcRaw) : null;
 
@@ -79,6 +79,26 @@ export const TeamAssignmentView = ({
               data-test-id="AssignmentView-deadline"
             >
               {deadlineUtc ? formatDateTimeLocal(deadlineUtc) : 'Не указан'}
+            </span>
+          </span>
+
+          <span className={styles.metaItem}>
+            <span className={styles.metaLabel}>Минимальная оценка:</span>
+            <span
+              className={styles.metaValue}
+              data-test-id="AssignmentView-minMark"
+            >
+              {minMark ? minMark : 'Не указана'}
+            </span>
+          </span>
+
+          <span className={styles.metaItem}>
+            <span className={styles.metaLabel}>Максимальная оценка:</span>
+            <span
+              className={styles.metaValue}
+              data-test-id="AssignmentView-maxMark"
+            >
+              {maxMark ? maxMark : 'Не указана'}
             </span>
           </span>
         </div>
